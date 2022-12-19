05f0668ef2d5a363e0b627552f4a6b55c780f003
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-12-19 17:05:00 +0000

    payment bug

--- booking-project/src/pots/Landing/Checkout.jsx
+++ booking-project/src/pots/Landing/Checkout.jsx
@@ -7,6 +7,8 @@ import { useParams } from "react-router-dom";
 // import Layout from "../../components/Layout";
 import axios from "axios";
 import toast,{Toaster} from "react-hot-toast";
+import { FaRestroom } from "react-icons/fa";
+import Layout from "../../components/Layout";
 
 const Checkout = () => {
   const router = useNavigate();
@@ -14,18 +16,16 @@ const Checkout = () => {
   const [sendPaymentDetails, setSendPaymentDetails] = useState(false);
   const [transactionRef, setTransactionRef] = useState(false);
   const { id } = useParams();
-
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
-  
-
+  const amount = detail.apartmentPrice;
 
 
 
   const getSingleDetail = async () => {
     let reqOptions = {
-      url: `http://localhost:8000/api/room/${id}/`,
+      url: `https://saferoyalhostel.pythonanywhere.com/api/room/${id}/`,
       method: "GET",
     };
 
@@ -38,14 +38,6 @@ const Checkout = () => {
     getSingleDetail();
   }, []);
 
-  // let bodyContent = new FormData();
-  //       bodyContent.append("fullname", name);
-  //       bodyContent.append("email", email);
-  //       bodyContent.append("password", password);
-  //       bodyContent.append("phone_number", phone);
-    
-  //  transNotify = () => 
-  //  toast.success( `Transaction Successful ${transaction.reference}`);
 
  
   const paywithPaystack = (e) => {
@@ -55,13 +47,13 @@ const Checkout = () => {
     paystack.newTransaction({
       key: "pk_live_d81027f6c5f51a74c46a68851be59da7df6f7b3a",
       email: email,
-      amount: amount,
+      amount: amount * 100,
 
       //Successful payment
       onSuccess(transaction) {
         let msg = `Transaction Successful ${transaction.reference}`;
         toast.success(msg);
-          
+
         router("/success");
         setSendPaymentDetails(true);
         setTransactionRef(transaction.ref);
@@ -103,75 +95,77 @@ const Checkout = () => {
     }
   }, [sendPaymentDetails]);
 
-
+ 
  
   return (
     <div>
-     
-      <Toaster />
-      <main>
-        <div className="flex justify-center items-center">
-          <div className="py-16 px-4 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container ">
-            <div className="flex flex-col justify-start items-start w-full space-y-9">
-              <div className="flex flex-col xl:flex-row justify-center xl:justify-between space-y-6 xl:space-y-0 xl:space-x-6 w-full ">
-                <div className=" flex flex-col sm:flex-row xl:flex-col justify-center items-center   sm:py-0 xl:py-10 px-10 xl:w-full">
-                  <img src={detail.room1} alt="" />
-                </div>
-
-                <form className="p-8 bg-gray-100 flex flex-col lg:w-full xl:w-3/5 ">
-                  <label className="mt-8 text-base leading-4 text-gray-800">
-                    Name
-                  </label>
-                  <div className="mt-8">
-                    <input
-                      className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600"
-                      type="text"
-                      id="name"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                      placeholder="Full name"
-                    />
-                  </div>
-                  <label className="mt-8 text-base leading-4 text-gray-800">
-                    Email
-                  </label>
-                  <div className="mt-8">
-                    <input
-                      className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600"
-                      type="email"
-                      placeholder="Email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </div>
-                  <label className="mt-8 text-base leading-4 text-gray-800">
-                    Phone
-                  </label>
-                  <div className="mt-8">
-                    <input
-                      className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600"
-                      type="phone"
-                      placeholder="Phone"
-                      value={phone}
-                      onChange={(e) => setPhone(e.target.value)}
-                    />
+      <Layout>
+        <main>
+          <div className="flex justify-center items-center">
+            <div className="py-16 px-4 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container ">
+              <div className="flex flex-col justify-start items-start w-full space-y-9">
+                <div className="flex flex-col xl:flex-row justify-center xl:justify-between space-y-6 xl:space-y-0 xl:space-x-6 w-full ">
+                  <div className=" flex flex-col sm:flex-row xl:flex-col justify-center items-center   sm:py-0 xl:py-10 px-10 xl:w-full">
+                    <img src={detail.room1} alt="pic" />
                   </div>
 
-                  <button
-                    type="submit"
-                    onClick={paywithPaystack}
-                    className="mt-8 border border-transparent hover:border-gray-300 bg-gray-900 hover:bg-white text-white hover:text-gray-900 flex justify-center items-center py-4 rounded w-full"
-                  >
-                    <div>
-                      <p className="text-base leading-4">Pay Ghc{detail.apartmentPrice}</p>
+                  <form className="p-8 bg-gray-100 flex flex-col lg:w-full xl:w-3/5 ">
+                    <label className="mt-8 text-base leading-4 text-gray-800">
+                      Name
+                    </label>
+                    <div className="mt-8">
+                      <input
+                        className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Full name"
+                      />
                     </div>
-                  </button>
-                </form>
+                    <label className="mt-8 text-base leading-4 text-gray-800">
+                      Email
+                    </label>
+                    <div className="mt-8">
+                      <input
+                        className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                    <label className="mt-8 text-base leading-4 text-gray-800">
+                      Phone
+                    </label>
+                    <div className="mt-8">
+                      <input
+                        className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                        type="phone"
+                        placeholder="Phone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                      />
+                    </div>
+
+                    <button
+                      type="submit"
+                      onClick={paywithPaystack}
+                      className="mt-8 border border-transparent hover:border-gray-300 bg-gray-900 hover:bg-white text-white hover:text-gray-900 flex justify-center items-center py-4 rounded w-full"
+                    >
+                      <div>
+                        <p className="text-base leading-4">
+                          Pay Ghc {detail.apartmentPrice}{" "}
+                        </p>
+                      </div>
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </Layout>
     </div>
   );
 };
