45a6f3f63c313509dc0bf5275b5d60d6b18cf314
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-12-23 08:59:30 +0000

    contact

--- booking-project/src/components/Form.jsx
+++ booking-project/src/components/Form.jsx
@@ -6,28 +6,28 @@ import  { useRef } from "react";
 const Form = () => {
  const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_5cjas6v",
-        "template_e6tnhav",
-        form.current,
-        "pkNjH6apHSrzAWYkA"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
+  //   emailjs
+  //     .sendForm(
+  //       "service_5cjas6v",
+  //       "template_e6tnhav",
+  //       form.current,
+  //       "pkNjH6apHSrzAWYkA"
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
 
-          alert("Message sent, We will contact you shortly");
-          form.current.reset();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+  //         alert("Message sent, We will contact you shortly");
+  //         form.current.reset();
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  // };
   return (
     <div>
       <section className="text-gray-600 body-font relative">
@@ -40,7 +40,7 @@ const Form = () => {
               We're available around the clock. Let us know how we can help!
             </p>
           </div>
-          <form ref={form} onSubmit={sendEmail}>
+          <form onSubmit={""}>
             <div className="lg:w-1/2 md:w-2/3 mx-auto">
               <div className="flex flex-wrap -m-2">
                 <div className="p-2 w-1/2">
@@ -91,13 +91,15 @@ const Form = () => {
                   </div>
                 </div>
                 <div className="p-2 w-full">
-                  <button
-                    type="submit"
-                    className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
-                    value="Send"
-                  >
-                    Send
-                  </button>
+                  <a href="powersam360@gmail.com">
+                    <button
+                    
+                      className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
+                      value="Send"
+                    >
+                      Send
+                    </button>
+                  </a>
                 </div>
               </div>
             </div>
